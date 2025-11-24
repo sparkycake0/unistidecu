30332869caf64f75285d695ac6abbc7550da73f5
lukasin2488 <lodjatube@gmail.com>
2025-11-24 23:24:04 +0100

    moje ime

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { motion } from "framer-motion";
 export default function Home() {
   return (
     <main>
-      <div>asdasd</div>
+      <div className="w-full h-screen flex justify-center text-3xl">
+        <motion.h1
+          initial={{ y: "-100%", opacity: 0, scale: 0.2 }}
+          animate={{ y: [0, 100, 100], opacity: 100, scale: 1 }}
+          transition={{ duration: 1 }}
+        >
+          Zrdavo deco
+        </motion.h1>
+      </div>
     </main>
   );
 }
